3513255aa9b59e7b47182f79da04816db2aac423
Deborah Ihesiaba <deborahihesiaba@gmail.com>
2022-11-10 16:18:05 +0100

    Listed all commands used on “command.sh” file

--- src/command.sh
+++ src/command.sh
@@ -1,1 +1,16 @@
+npx create-react-app blackbird
+npm run build
+git add -A
+git commit -m 'Changed logo in the head element'
+git push https://github.com/DeborahIhesiaba/blackbird
+git branch update_logo
+git checkout update_logo
+git add -A
+git commit -m 'Replaced existin
+g logo and link'
+git push -u origin update_logo
+git checkout master
+git merge update_logo
+git push origin master
+
 https://github.com/DeborahIhesiaba/blackbird
